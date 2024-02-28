3a77597c88499676f6a8e8056b3c5ee8bd5b4e21
ari.thomazini <ari.thomazini@gmail.com>
2024-02-28 20:54:41 -0300

    logout

--- src/main/java/br/senac/familyhotdog/hotdog/controller/IndexController.java
+++ src/main/java/br/senac/familyhotdog/hotdog/controller/IndexController.java
@@ -2,7 +2,12 @@ package br.senac.familyhotdog.hotdog.controller;
 
 import br.senac.familyhotdog.hotdog.dao.BoloRepository;
 import br.senac.familyhotdog.hotdog.model.Bolo;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,5 +27,14 @@ public class IndexController {
         model.addAttribute("bolos", bolos);
 
         return "index";
+    }
+
+    @GetMapping(value="/logout")
+    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/";
     }
 }
